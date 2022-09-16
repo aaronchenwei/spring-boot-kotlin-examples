038dc89a20b127b9c371fe0bbaf73085ab55cd7c
aaronchenwei <aaronchenwei@hotmail.com>
2022-09-16 20:45:50 +0800

    reformat code

--- hello-world-web/src/main/kotlin/io/github/aaronchenwei/springboot/example/HelloWorldWebApplication.kt
+++ hello-world-web/src/main/kotlin/io/github/aaronchenwei/springboot/example/HelloWorldWebApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class HelloWorldWebApplication
 
 fun main(args: Array<String>) {
-    runApplication<HelloWorldWebApplication>(*args)
+  runApplication<HelloWorldWebApplication>(*args)
 }

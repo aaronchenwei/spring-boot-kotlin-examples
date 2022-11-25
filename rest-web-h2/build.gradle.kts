09cbcd7a14a8a63c69904289dc015f6c78f19352
aaronchenwei <aaronchenwei@hotmail.com>
2022-11-25 09:10:56 +0800

    spring boot 3.0.0

--- rest-web-h2/build.gradle.kts
+++ rest-web-h2/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.springframework.boot") version "3.0.0-RC2"
-  id("io.spring.dependency-management") version "1.0.14.RELEASE"
+  id("org.springframework.boot") version "3.0.0"
+  id("io.spring.dependency-management") version "1.1.0"
   kotlin("jvm") version "1.7.21"
   kotlin("plugin.spring") version "1.7.21"
   kotlin("plugin.jpa") version "1.7.21"
@@ -14,7 +14,6 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
   mavenCentral()
-  maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 dependencies {
--- webflux-r2dbc-h2/build.gradle.kts
+++ webflux-r2dbc-h2/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.springframework.boot") version "3.0.0-RC2"
+  id("org.springframework.boot") version "3.0.0"
   id("io.spring.dependency-management") version "1.1.0"
   kotlin("jvm") version "1.7.21"
   kotlin("plugin.spring") version "1.7.21"
@@ -13,7 +13,6 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
   mavenCentral()
-  maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 dependencies {

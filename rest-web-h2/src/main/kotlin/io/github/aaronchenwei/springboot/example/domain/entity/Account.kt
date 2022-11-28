de5a5044aba2195093377a777b530f8aedd3322b
aaronchenwei <aaronchenwei@hotmail.com>
2022-11-28 13:23:35 +0800

    Change javax.persistence.* to jakarta.persistence.*

--- rest-web-h2/src/main/kotlin/io/github/aaronchenwei/springboot/example/domain/entity/Account.kt
+++ rest-web-h2/src/main/kotlin/io/github/aaronchenwei/springboot/example/domain/entity/Account.kt
@@ -1,8 +1,8 @@
 package io.github.aaronchenwei.springboot.example.domain.entity
 
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Table
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "account")

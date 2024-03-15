65c886437e9bc6e4ffd8daf927ea86fecce7a6f9
aaronchenwei <aaronchenwei@users.noreply.github.com>
2024-03-15 17:52:36 +0800

    Update versions

--- rest-web-h2/build.gradle.kts
+++ rest-web-h2/build.gradle.kts
@@ -1,16 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.springframework.boot") version "3.0.5"
-  id("io.spring.dependency-management") version "1.1.0"
-  kotlin("jvm") version "1.8.10"
-  kotlin("plugin.spring") version "1.8.10"
-  kotlin("plugin.jpa") version "1.8.10"
+  id("org.springframework.boot") version "3.2.3"
+  id("io.spring.dependency-management") version "1.1.4"
+  kotlin("jvm") version "1.9.22"
+  kotlin("plugin.spring") version "1.9.22"
+  kotlin("plugin.jpa") version "1.9.22"
 }
 
 group = "io.github.aaronchenwei.springboot.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
   mavenCentral()
@@ -23,7 +26,6 @@ dependencies {
   runtimeOnly("com.h2database:h2")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
